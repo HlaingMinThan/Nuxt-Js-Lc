d45ea30b3c7ca3d0e40b9f798e19ebb8a1764878
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-08-25 22:00:55 +0630

    client side fetching

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,22 @@
     <!-- Lazy Loading component -->
     <LazyFooBarBaz/> 
     <h1>Hello</h1>
+    <div>
+        <ul>
+            <li v-for="user in users" :key="user.id">{{user.name}}</li>
+        </ul>
+    </div>
 </template>
 
-<script>
+<script setup>
+let users = ref([]);
+
+//client side fetching (not generate html from server - no ssr)
+onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json()).then((data) => {
+        users.value = data;
+    })
+})
 // This will work in both `<script setup>` and `<script>`
 definePageMeta({
   layout: "custom",
