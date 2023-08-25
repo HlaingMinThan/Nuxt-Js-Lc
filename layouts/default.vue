8fddd43f0a16873d11fdfcdc594fae197d3c1072
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-08-25 14:09:50 +0630

    default layout system

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Posts</a></li>
+        </ul>
+    </nav>
+    <slot />
+    <footer>
+         <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Posts</a></li>
+        </ul>
+    </footer>
+  </div>
+</template>
